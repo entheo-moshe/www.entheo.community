b0c2a00fadb08c03d61a8eb06978a076c2f5bade
Moshe Jacobson <moshe@neadwerx.com>
2026-06-09 15:36:18 -0400

    second attempt at fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const config = defineConfig({
+export default defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [tanstackRouter({ target: 'react' }), tailwindcss(), viteReact()],
 })
-
-export default config
